21ffe79c80809f1c5088cdf1a1485f94fe59ef6c
dzhang <zhangda.sjtu@gmail.com>
2016-03-03 00:26:24 -0800

    Add RESTful support

--- base_project/base/src/main/java/cs263w16/datastore/Message.java
+++ base_project/base/src/main/java/cs263w16/datastore/Message.java
@@ -1,67 +1,89 @@
 package cs263w16.datastore;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 import java.util.*;
 
 @XmlRootElement
-// JAX-RS supports an automatic mapping from JAXB annotated class to XML and JSON
+// JAX-RS supports an automatic mapping from JAXB annotated class to XML and
+// JSON
 public class Message {
-  private String usr_name;
-  private String content;
-  private Date date;
-  private double longitude;
-  private double latitude;
+	private String usr_name;
+	private String content;
+	private int type;
+	private Date date;
+	private double longitude;
+	private double latitude;
+	private String options;
 
-public Message(){
-	
-}
+	public Message() {
 
-public Message(String usr_name, String content, Date date, double longitude,
-		double latitude) {
-	this.usr_name = usr_name;
-	this.content = content;
-	this.date = date;
-	this.longitude = longitude;
-	this.latitude = latitude;
-}
+	}
 
-public String getUsr_name() {
-	return usr_name;
-}
+	public Message(String usr_name, String content, int type, Date date,
+			double longitude, double latitude, String options) {
+		this.usr_name = usr_name;
+		this.content = content;
+		this.type = type;
+		this.date = date;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.options = options;
+	}
 
-public void setUsr_name(String usr_name) {
-	this.usr_name = usr_name;
-}
+	public int getType() {
+		return type;
+	}
 
-public String getContent() {
-	return content;
-}
+	public void setType(int type) {
+		this.type = type;
+	}
 
-public void setContent(String content) {
-	this.content = content;
-}
+	public String getOptions() {
+		return options;
+	}
 
-public Date getDate() {
-	return date;
-}
+	public void setOptions(String options) {
+		this.options = options;
+	}
 
-public void setDate(Date date) {
-	this.date = date;
-}
+	public String getUsr_name() {
+		return usr_name;
+	}
 
-public double getLongitude() {
-	return longitude;
-}
+	public void setUsr_name(String usr_name) {
+		this.usr_name = usr_name;
+	}
 
-public void setLongitude(double longitude) {
-	this.longitude = longitude;
-}
+	public String getContent() {
+		return content;
+	}
 
-public double getLatitude() {
-	return latitude;
-}
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-public void setLatitude(double latitude) {
-	this.latitude = latitude;
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
 }
-} 
